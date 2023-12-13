d3a315d901ccecbff558a82a85dd9cf457ab7be7
SrDaviid <luis.david.08@hotmail.com>
2023-12-13 10:45:32 -0400

    implement router

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:news_world_app/screens/auth/login_screen.dart';
+import 'package:news_world_app/screens/home_screen.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/login',
+  routes: <GoRoute>[
+    GoRoute(
+      path: '/login',
+      name: LoginScreen.name,
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: '/home',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
