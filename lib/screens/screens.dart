def9f5d03ad99d0dc963136c24a8d7e3a86dc09e
SrDaviid <luis.david.08@hotmail.com>
2023-12-14 11:30:52 -0400

    Create screens file

---
+++ lib/screens/screens.dart
@@ -0,0 +1,6 @@
+export 'package:news_world_app/screens/pages/newspaper_screen.dart';
+export 'package:news_world_app/screens/auth/login_screen.dart';
+export 'package:news_world_app/screens/home_screen.dart';
+export 'package:news_world_app/screens/pages/headers_news_screen.dart';
+
+
