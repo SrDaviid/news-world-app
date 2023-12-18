991baedd5650d1f488ec6104bc4aad7ab5d6d2d0
SrDaviid <luis.david.08@hotmail.com>
2023-12-18 08:15:35 -0400

    Create "NewsService" and add "getTopHeadlines function" with API endpoint

---
+++ lib/services/news_service.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:news_world_app/config/environment/environment.dart';
+import 'package:news_world_app/models/news_models.dart';
+import 'package:http/http.dart' as http;
+
+class NewsService extends ChangeNotifier {
+  final _NEWS_API = "https://newsapi.org/v2/";
+  final _API_KEY = Environment.newsAPIkey;
+  List<Article> headlines = [];
+
+  NewsService() {
+    this.getTopHeadlines();
+  }
+
+  getTopHeadlines() async {
+    final url =
+        Uri.parse("$_NEWS_API/top-headlines?country=us&apiKey=$_API_KEY");
+
+    final resp = await http.get(url);
+
+    final newsResponse = newsResponseFromJson(resp.body);
+    this.headlines.addAll(newsResponse.articles);
+    print(newsResponse.articles[3].title);
+    notifyListeners();
+  }
+}
