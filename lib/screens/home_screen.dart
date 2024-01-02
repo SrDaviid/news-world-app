48b4b4477bc2467fd252b01518cc4f8821eaf1b7
SrDaviid <luis.david.08@hotmail.com>
2024-01-02 10:53:43 -0400

    add KeepAliveClient to keep the current screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:news_world_app/screens/screens.dart';
-import 'package:news_world_app/services/news_service.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const name = 'home-screen';
@@ -11,26 +9,21 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
   int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    final newsService = Provider.of<NewsService>(context);
+    super.build(context);
     final List<Widget> screens = [
       const NewspaperScreen(),
       const HeaderNewsScreen(),
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Home screen',
-          style: TextStyle(fontWeight: FontWeight.w500),
-        ),
-      ),
       body: screens[selectedIndex],
-      backgroundColor: Colors.indigo,
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 4.3,
         currentIndex: selectedIndex,
         onTap: (index) {
           setState(
@@ -41,17 +34,21 @@ class _HomeScreenState extends State<HomeScreen> {
         },
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(Icons.public_sharp),
-              label: 'News',
-              activeIcon: Icon(Icons.public_rounded)),
+            icon: Icon(Icons.public_sharp),
+            label: 'News',
+            activeIcon: Icon(Icons.public_rounded),
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.newspaper_outlined),
             label: 'Headers',
             activeIcon: Icon(Icons.newspaper_rounded),
           ),
         ],
-        selectedItemColor: Colors.red,
+        selectedItemColor: Colors.indigo,
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
