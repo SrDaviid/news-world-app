69ec9841bc174ade71b12d5d06861d1882fad04a
SrDaviid <luis.david.08@hotmail.com>
2024-01-09 08:59:31 -0400

    Refactor news service

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,20 +1,21 @@
-import 'package:flutter/material.dart';
 import 'package:news_world_app/config/environment/environment.dart';
-import 'package:news_world_app/models/news_models.dart';
+import 'package:news_world_app/models/models.dart';
 import 'package:http/http.dart' as http;
 
+// ignore: prefer_const_declarations
+final _newsApi = "https://newsapi.org/v2/";
+final _apiKey = Environment.newsAPIkey;
+
 class NewsService extends ChangeNotifier {
-  final _newsApi = "https://newsapi.org/v2/";
-  final _apiKey = Environment.newsAPIkey;
   List<Article> headlines = [];
+  List<Category> categories = [];
 
   NewsService() {
     getTopHeadlines();
   }
 
   getTopHeadlines() async {
-    final url =
-        Uri.parse("$_newsApi/top-headlines?country=us&apiKey=$_apiKey");
+    final url = Uri.parse("$_newsApi/top-headlines?country=us&apiKey=$_apiKey");
 
     final resp = await http.get(url);
 
