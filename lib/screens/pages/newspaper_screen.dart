e5f8655658de1e10a52431ffac7b6e4f9cf47e32
SrDaviid <luis.david.08@hotmail.com>
2024-01-09 09:21:09 -0400

    Design "NewsPaperScreen" done

--- lib/screens/pages/newspaper_screen.dart
+++ lib/screens/pages/newspaper_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_world_app/widgets/list_news.dart';
 import 'package:provider/provider.dart';
 import '../../services/news_service.dart';
 
@@ -8,6 +10,16 @@ class NewspaperScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final newsService = Provider.of<NewsService>(context);
-    return const Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'News',
+            style: GoogleFonts.nunito(
+                fontWeight: FontWeight.w800, color: Colors.white, fontSize: 24),
+          ),
+          backgroundColor: Colors.indigo,
+        ),
+        body: ListNews(newsService.headlines));
   }
 }
