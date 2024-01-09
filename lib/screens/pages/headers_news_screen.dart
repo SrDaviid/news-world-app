34cb4ce5690db516a34c38e4cadc13824c5f6497
SrDaviid <luis.david.08@hotmail.com>
2024-01-09 08:58:45 -0400

    Add centerTitle to the appBar

--- lib/screens/pages/headers_news_screen.dart
+++ lib/screens/pages/headers_news_screen.dart
@@ -9,6 +9,7 @@ class HeaderNewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text(
           'Headers',
           style: GoogleFonts.nunito(
